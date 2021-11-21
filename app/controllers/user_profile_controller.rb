196747a66c4c821d579f9ff9e2485884112f86c2
Sergey Arenzon <sergeyr1991@gmail.com>
2021-11-21 18:34:15 +0200

    test

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -8,12 +8,13 @@ class UserProfileController < BasicAuthController
 
   def update_user_results
     user_feedback  = []
-    pp "========"
-    pp paramas
-    pp "========"
     params[:user_results].each do |b|
       Result.find_by(user_profile_id:  params[:user_id], product_id: b['product_id']).update(feedback: b['garment_fit'])
-      user_feedback.push("#{b['size_chart_name']} : #{b['garment_fit']}")
+
+      product = Product.find_by(id: b['product_id']);
+      category = Category.find_by(id: product.category_id);
+
+      user_feedback.push("#{category_name} - #{}#{b['size_chart_name']} : #{b['garment_fit']}")
     end
     UserProfile.find_by(email: params[:external_id]).update(user_feedback: user_feedback.join("\n"), other_feedback: params[:feedback])
   end
