e675ce0b69cbf6a3cc2585b6c0e8fb736a8f1542
Sergey Arenzon <sergeyr1991@gmail.com>
2021-11-24 10:19:03 +0200

    change products view & remove user feedback

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -12,7 +12,29 @@ class ProductController < ApplicationController
     size_chart_codes = @products.pluck(:size_chart_code).uniq.join(',')
     pp "#{ENV['BACKEND_URL']}/sdk/external_users/me/garment_sizes?size_chart_codes_csv=#{size_chart_codes}&external_id=#{params[:external_id]}&sdk_key=#{ENV['SDK_KEY']}&sdk_secret=#{ENV['SDK_SECRET']}"
     @size_response = HTTParty.get("#{ENV['BACKEND_URL']}/sdk/external_users/me/garment_sizes?size_chart_codes_csv=#{size_chart_codes}&external_id=#{params[:external_id]}&sdk_key=#{ENV['SDK_KEY']}&sdk_secret=#{ENV['SDK_SECRET']}")
-    pp @size_response
+  
+    arr = []
+    @products.each_with_index do |product, index|
+      category_id = product[:category_id]
+      arr.push({
+        product_id: product[:id], 
+        size: @size_response[index]["size"],
+        category_id: product[:category_id],
+        title: product[:title],
+        logo: product[:logo]
+         })
+     
+    end
+
+   arr = arr.group_by{ |product| product[:category_id]}
+
+   @arr_res = []
+   arr.each do |key,value|
+    category = Category.find_by(id: key).name;
+    @arr_res << {category: category, products: value}
+   end
+
+
     @gender = params[:gender]
     @external_id = params[:external_id]
     @products.each do |product|
@@ -20,12 +42,15 @@ class ProductController < ApplicationController
         if sr['size_chart_code'] == product.size_chart_code
          category = Category.find_by(id: product.category_id);
           @product_res << {title: product.title, logo: product.logo, size: sr['size'], product_id: product.id, category_name: category&.name}
+          
         end
       }
     end
+   
     @product_res.each do |b|      
       user_results.push("#{b[:category_name]} - #{b[:title]} : #{b[:size]}")
     end
+
 
 
     user.update(user_results: user_results.join("\n"))
@@ -33,5 +58,10 @@ class ProductController < ApplicationController
       pp prod_res
       Result.create(user_profile_id: user.id, product_id: prod_res[:product_id], results: prod_res[:size])
     end
+
+
+    pp "]]]]]]]]]]"
+    pp @arr_res
+    pp "]]]]]]]]]]"
   end
 end
--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -3,17 +3,18 @@ class UserProfileController < BasicAuthController
   layout "tesla"
 
   def index
-
+    
   end
 
   def update_user_results
-    user_feedback  = []
-    params[:user_results].each do |b|
-      Result.find_by(user_profile_id:  params[:user_id], product_id: b['product_id']).update(feedback: b['garment_fit'])
-      category = Product.find(b['product_id'])&.category
-      user_feedback.push("#{category&.name} - #{b['size_chart_name']} : #{b['garment_fit']}")
-    end
-    UserProfile.find_by(email: params[:external_id]).update(user_feedback: user_feedback.join("\n"), other_feedback: params[:feedback])
+    # user_feedback  = []
+    # params[:user_results].each do |b|
+    #   Result.find_by(user_profile_id:  params[:user_id], product_id: b['product_id']).update(feedback: b['garment_fit'])
+    #   category = Product.find(b['product_id'])&.category
+    #   user_feedback.push("#{category&.name} - #{b['size_chart_name']} : #{b['garment_fit']}")
+    # end
+    # UserProfile.find_by(email: params[:external_id]).update(user_feedback: user_feedback.join("\n"), other_feedback: params[:feedback])
+    UserProfile.find_by(email: params[:external_id]).update(other_feedback: params[:feedback])
   end
 
 
